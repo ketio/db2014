ac5fe9d453a803ad4d313b5a934b0646509f770b
weichunliao <weichun5795@gmail.com>
2015-01-17 14:35:20 +0800

    ~~

--- login.php
+++ login.php
@@ -2,11 +2,41 @@
 <head>
 </head>
 
-<body>
-<div>
-Sign In
-</div>
+<style>
+#title{
+	width: 150px;
+	height: 30px;
+	background-color: rgb(179, 178, 178);
+	
+	text-align: right;
+	font-size: 25px;
+	font-family: Arial, Tahoma;
+}
 
+</style>
+
+
+<body>
+	
+	<div style="width: 500px; height: 400px">
+		<div id="title">
+			Sign In
+		</div>
+		
+		<div>
+			ACCOUNT | 會員帳號
+			<input>
+			</input>
+		</div>
+		<div>
+			PASSWORD | 會員密碼
+			<input>
+			</input>
+		</div>
+		<div>
+			登入
+		</div>
+	</div>
 </body>
 
 </html>
