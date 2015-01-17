38a0feb201ca28f7c15a524e6749d1a708e080af
Geng Yu <liveman24224@gmail.com>
2015-01-17 14:38:31 +0800

    heee

--- login.php
+++ login.php
@@ -1,42 +1,63 @@
 <html>
 <head>
-</head>
-
-<style>
-#title{
-	width: 150px;
-	height: 30px;
-	background-color: rgb(179, 178, 178);
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>NTUIM CLOULD THEATER雲端影城</title>
+	<script type="text/javascript" src="system/js/jquery-1.11.2.min"></script>
+	<link type="text/css" rel="stylesheet" href="css/login.css"/>
 	
-	text-align: right;
-	font-size: 25px;
-	font-family: Arial, Tahoma;
-}
+	<script>
+		$(document).ready(function () {
 
-</style>
+		});
+	</script>	
+	</head>
 
-
-<body>
-	
-	<div style="width: 500px; height: 400px">
-		<div id="title">
-			Sign In
-		</div>
+	<style>
+	#title{
+		width: 150px;
+		height: 30px;
+		background-color: rgb(179, 178, 178);
 		
-		<div>
-			ACCOUNT | 會員帳號
-			<input>
-			</input>
+		text-align: right;
+		font-size: 25px;
+		font-family: Arial, Tahoma;
+	}
+	#login_page{
+		background:white;
+		height:1000px;
+	}
+
+	</style>
+
+
+	<body>
+		<div id="content_wrapper">
+			<!-- header -->
+			<?php 
+				include_once "header.php"; 
+			?>	
+			<div id="login_page">
+				<div style="width: 500px; height: 400px">
+					<div id="title">
+						Sign In
+					</div>
+					
+					<div>
+						ACCOUNT | 會員帳號
+						<input>
+						</input>
+					</div>
+					<div>
+						PASSWORD | 會員密碼
+						<input>
+						</input>
+					</div>
+					<div>
+						登入
+					</div>
+				</div>
+			</div>
 		</div>
-		<div>
-			PASSWORD | 會員密碼
-			<input>
-			</input>
-		</div>
-		<div>
-			登入
-		</div>
-	</div>
-</body>
+	</body>
 
 </html>
