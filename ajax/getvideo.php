30bc39f2894d28fcbc5dd3c6f0e6645edf1e9e81
Geng Yu <liveman24224@gmail.com>
2015-01-19 02:27:53 +0800

    dataaa

--- ajax/getvideo.php
+++ ajax/getvideo.php
@@ -130,6 +130,121 @@
 			echo "</videos>";				
 		echo "</response>";
 	}
+	if($mode=="search"){
+		$result=array();
+		$query=
+			"select ".
+				"A.videoID,A.videoName,C.videoTypeName,C.videoTypeMode,A.rentPrice,A.buyPrice,A.publishDate,B.publisherName,B.publisherCountry,A.lang,A.intro ".
+			"from ".
+				"video as A, ".
+				"publisher as B, ".
+				"videotype as C ".
+			"where ".
+				"B.publisherID = A.publisher and C.videoTypeID = A.videotype and C.videoTypeName= ? ";
+		if($videoTypeMode!="default"){
+			$query.=
+				" and C.videoTypeMode= ? ";
+		}
+		if($orderBy=="buyPrice"){
+			$query.=
+				"order by A.buyPrice";
+		}elseif($orderBy=="publishDate"){
+			$query.=
+				"order by A.publishDate";
+		}else{
+			//donothing
+		}
+		
+
+		$stmt = mysqli_prepare($mysqli, $query);
+
+		if($videoTypeMode=="default"){
+			mysqli_stmt_bind_param($stmt, "s",$videoTypeName);
+		}
+		elseif($videoTypeMode!="default"){
+			mysqli_stmt_bind_param($stmt, "ss",$videoTypeName,$videoTypeMode);
+		}
+			/* bind parameters for markers */
+		//mysqli_stmt_bind_param($stmt, "sss",$videoID,$videoName,$videoType);
+		mysqli_stmt_bind_result($stmt,$videoID,$videoName,$videoTypeName,$videoTypeMode,$rentPrice,$buyPrice,$publishDate,$publisherName,$publisherCountry,$lang,$intro);
+		mysqli_stmt_execute($stmt);
+		while(mysqli_stmt_fetch($stmt)){
+			$temp = array();
+			$temp["videoID"]=$videoID;
+			$temp["videoName"]=$videoName;
+			$temp["videoType"]=$videoTypeName."/".$videoTypeMode;
+			$temp["rentPrice"]=$rentPrice;
+			$temp["buyPrice"]=$buyPrice;
+			$temp["publishDate"]=$publishDate;
+			$temp["publisher"]=$publisherName."/".$publisherCountry;
+			$temp["lang"]=$lang;
+			$temp["intro"]=$intro;
+			
+			$mysqli2=connect_database();
+			$rating = 0;
+			$query2=
+				"select ".
+					"avg(rating) ".
+				"from ".
+					"user_feedback ".
+				"where ".
+					"videoID= ? ";
+			$stmt2 = mysqli_prepare($mysqli2, $query2);
+			mysqli_stmt_bind_param($stmt2,"s",$videoID);
+			mysqli_stmt_bind_result($stmt2,$ratingTemp);
+			mysqli_stmt_execute($stmt2);
+			if(mysqli_stmt_fetch($stmt2)){
+				$rating = $ratingTemp;
+			}			
+			mysqli_stmt_close($stmt2);
+			$temp["rating"]=$rating;
+			
+			array_push($result,$temp);
+		}
+		mysqli_stmt_close($stmt);
+
+		echo "<response>";
+			echo "<result>";
+				echo "取得成功";
+			echo "</result>";	
+			echo "<videos>";
+				foreach($result as $video){
+					echo "<video>";
+						echo "<videoID>";
+							echo $video["videoID"];
+						echo "</videoID>";	
+						echo "<videoName>";
+							echo $video["videoName"];
+						echo "</videoName>";
+						echo "<videoType>";
+							echo $video["videoType"];
+						echo "</videoType>";			
+						echo "<rentPrice>";
+							echo $video["rentPrice"];
+						echo "</rentPrice>";	
+						echo "<buyPrice>";
+							echo $video["buyPrice"];
+						echo "</buyPrice>";
+						echo "<publishDate>";
+							echo $video["publishDate"];
+						echo "</publishDate>";
+						echo "<publisher>";
+							echo $video["publisher"];
+						echo "</publisher>";
+						echo "<lang>";
+							echo $video["lang"];
+						echo "</lang>";		
+						echo "<intro>";
+							echo $video["intro"];
+						echo "</intro>";		
+						echo "<rating>";
+							echo $video["rating"];
+						echo "</rating>";						
+					echo "</video>";
+				}
+			echo "</videos>";				
+		echo "</response>";
+	}
 	elseif($mode=="item"){
 	
 		$islogin=false;
--- search.php
+++ search.php
@@ -1,21 +1,114 @@
 <?php 
 	include_once "./system/session.php"; 
 ?>	
+<?php
+	$keyword=$_GET["keyword"];
+	
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>NTUIM CLOULD THEATER雲端影城</title>
-	<script type="text/javascript" src="system/js/jquery-1.11.2.min"></script>
-	<link type="text/css" rel="stylesheet" href="css/rank.css"/>
+	<script src="system/js/jquery-1.11.2.min.js"></script>
+	<link type="text/css" rel="stylesheet" href="css/videolist.css"/>
+	<script type="text/javascript" src="system/js/raty/lib/jquery.raty.js"></script>
+	<link type="text/css" rel="stylesheet" href="system/js/raty/lib/jquery.raty.css"/>
 	
 	<script>
-		$(document).ready(function (){
+		var keyword = <?php echo "'".$keyword."'";  ?>;
 
+		orderBy="default";
+		
+		
+		$(document).ready(function () {
+			$(".orderByItem[orderByValue='"+orderBy+"']").addClass("orderByItem_choose");
+		
+			getVideoList(videoTypeName,"default",orderBy);
+			
+			$(".orderByItem").click(function(){
+				$(".orderByItem[orderByValue='"+orderBy+"']").removeClass("orderByItem_choose");				
+				orderBy = $(this).attr("orderByValue");
+				$(this).addClass("orderByItem_choose");
+				getVideoList(videoTypeName,"default",orderBy);
+			});
 		});
+				
+		function getVideoList(videoTypeName,videoTypeMode,orderBy){
+					
+			$.ajax({
+				async: true,
+				type: "post",
+				url: "ajax/getvideo.php",
+				dataType: "xml",
+				data:{
+					keyword:keyword,
+					orderBy:orderBy,
+					mode:"search",
+				},
+				success: function(response){
+				
+					$("#videolist_interface").empty();
+					//alert($(response).find("result").text());
+					//console.log(response);
+					
+					$(response).find("video").each(function(){
+			
+						var video={
+							videoID:$(this).children("videoID").text(),
+							videoName:$(this).children("videoName").text(),
+							videoType:$(this).children("videoType").text(),
+							rentPrice:$(this).children("rentPrice").text(),
+							buyPrice:$(this).children("buyPrice").text(),
+							publishDate:$(this).children("publishDate").text(),
+							publisher:$(this).children("publisher").text(),
+							lang:$(this).children("lang").text(),
+							intro:$(this).children("intro").text(),
+							rating:$(this).children("rating").text()
+						}
+						
+						$("#videolist_interface").append(
+							"<div class='videolist_item'>"+
+								"<div class='video_rating_star_cell' id='Rating_"+video.videoID+"' data-number='"+video.rating+"' ></div>"+
+								"<div class='videolist_item_image_cell'>"+
+									"<a href='video.php?videoid="+video.videoID+"'>"+
+										"<img class='videolist_item_image' src='http://"+<?php echo '"'.$_SERVER['HTTP_HOST'].'"'; ?>+"/db2014/data/cover/"+video.videoID+".png'>"+
+									"</a>"+
+								"</div>"+
+								"<div class='videolist_item_name'>"+video.videoName+"</div>"+
+								"<div class='videolist_item_rentPrice'>售 NT. "+video.buyPrice+"</div>"+
+								"<div class='videolist_item_buyPrice'>租 NT. "+video.rentPrice+"</div>"+
+							"</div>"
+						);	
+						
+						if(video.rating==0){
+							$("#Rating_"+video.videoID).append(
+								"<img alt='1' src='system/js/raty/lib/images/star-no.png' title='regular'>"+
+								"<img alt='2' src='system/js/raty/lib/images/star-no.png' title='regular'>"+
+								"<img alt='3' src='system/js/raty/lib/images/star-no.png' title='regular'>"+
+								"<img alt='4' src='system/js/raty/lib/images/star-no.png' title='regular'>"+
+								"<img alt='5' src='system/js/raty/lib/images/star-no.png' title='regular'>"							
+							);
+						}
+						else{
+							$("#Rating_"+video.videoID).raty({
+								path:"system/js/raty/lib/images",
+								readOnly: true,
+								score:video.rating,
+							});		
+						}
+					});
+				}
+				
+			});
+		}
+		
 	</script>	
 	</head>
-
+	
+	<style>
+	</style>
+		
 	<body>
 		<div id="content_wrapper">
 
@@ -23,9 +116,17 @@
 			<?php 
 				include_once "header.php"; 
 			?>	
-			<div id="rank_page">
-			
-			
+			<div id="videolist_page">
+				<div id="orderByrow">
+					<div class="orderByItem" orderByValue="default">預設排序</div> | 
+					<div class="orderByItem" orderByValue="rating">依評價排序</div> | 
+					<div class="orderByItem" orderByValue="buyPrice">依價格排序</div> | 
+					<div class="orderByItem" orderByValue="having">依購買人次</div> | 
+					<div class="orderByItem" orderByValue="publishDate">依出版時間</div> 
+				</div>
+				<div id="videolist_interface">
+					
+				</div>
 			</div>
 			<?php 
 				//include_once "footer	.php"; 
